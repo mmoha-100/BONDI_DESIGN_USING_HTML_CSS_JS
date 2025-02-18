dd4abf175606d090d0cbded4ae5fe17cca894e68
mmoha-100 <g1thubm0haammed@gmail.com>
2025-02-18 17:02:39 +0300

    Update bondi.js

--- 5- First Bootstrap Design/js/bondi.js
+++ 5- First Bootstrap Design/js/bondi.js
@@ -2,13 +2,54 @@
 const navLinks = document.querySelectorAll(".navbar .nav-link");
 
 navLinks.forEach((navLink) => {
+    chooseActiveLink(navLink);
+});
+
+const homeSection = document.querySelector(".landing");
+const sectionOne = document.getElementById("services");
+const sectionTwo = document.getElementById("portfolio");
+const sectionThree = document.getElementById("about");
+const sectionFour = document.querySelector(".our-team");
+
+window.addEventListener("scroll", () => {
+    let height = window.scrollY;
+    if (height >= homeSection.offsetTop && height < sectionOne.offsetHeight) {
+        chooseActiveLink2(0);
+    } else if (
+        height >= sectionOne.offsetTop &&
+        height < sectionTwo.offsetTop
+    ) {
+        chooseActiveLink2(1);
+    } else if (
+        height >= sectionTwo.offsetTop &&
+        height < sectionThree.offsetTop
+    ) {
+        chooseActiveLink2(2);
+    } else if (
+        height >= sectionThree.offsetTop &&
+        height < sectionFour.offsetTop
+    ) {
+        chooseActiveLink2(3);
+    } else {
+        chooseActiveLink2(4);
+    }
+});
+
+function chooseActiveLink2(order) {
+    navLinks.forEach((l) => {
+        l.classList.remove("active");
+    });
+    navLinks[order].classList.add("active");
+}
+
+function chooseActiveLink(navLink) {
     navLink.addEventListener("click", () => {
         navLinks.forEach((l) => {
             l.classList.remove("active");
         });
         navLink.classList.add("active");
     });
-});
+}
 // End Navbar
 
 // Start Our Works
@@ -46,9 +87,7 @@ function addImgs(chosenCategory) {
         imgsContainer.append(img);
     });
 }
-// End Our Works
 
-// Shared Functions
 function whoActiveCategory(btn) {
     let all = btn.parentElement.children;
     Array.from(all).forEach((el) => {
@@ -56,3 +95,4 @@ function whoActiveCategory(btn) {
     });
     btn.className = "active rounded-pill";
 }
+// End Our Works
